db2128e4ff3ce733b94a58dface08b99b0ce7d10
ncvg <jmgvicen@gmail.com>
2017-12-19 21:12:35 +0100

    perceptro.py added

---
+++ perceptron.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class Perceptron(object):
+
+	def __init__(self, eta=0.01, n_iter=10):
+		#eta es el ratio de aprendizaje
+		self.eta = eta
+		#numero de iteraciones
+		self.n_iter = n_iter
+
+	def fit(self, X, y):
+		self.w_ = np.zeros(1 + X.shape[1])
+		self.errors_ = []
+
+		for _ in range(self.n_iter):
+			errors = 0
+
+			for xi, target in zip(X, y):
+				update = self.eta * (target - self.predict(xi))
+				self.w_[1:] += update * xi
+				self.w_[0] += update
+				errors += int(update != 0.0)
+
+			self.errors_.append(errors)
+
+		return self
+
+	def net_input(self, X):
+		#calcular el net input
+		return np.dot(X, self.w_[1:]) + self.w_[0]
+
+	def predict(self, X):
+		#devuelve la clase label despues del paso de unidad
+		return np.where(self.net_input(X) >= 0.0, 1 -1)
